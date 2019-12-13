d1638813bf217cae3a1a6ab46abb55b576d0cf6e
Adebayo Ilerioluwa <46798106+adebayoileri@users.noreply.github.com>
2019-12-13 12:40:21 +0100

    Mimicking user home

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,4 +5,8 @@ app.get('/routes',(req, res)=>{
     res.json('All routes files')
 });
 
+app.get('/dashboard/:name', (req,res)=>{
+    res.json(`Welcome Mr/Mrs ${req.params.name} to simple node app`);
+})
+
 module.exports = app;
